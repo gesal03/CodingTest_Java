5d4a829b0898fe31f7561273b5f2c989cfb63be9
Hyun seung Lee <gesal0303@gmail.com>
2024-01-31 15:42:02 +0900

    [Silver III] Title: 주유소, Time: 372 ms, Memory: 34448 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -7,25 +7,30 @@ import java.util.*;
 public class Main {
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private StringTokenizer st;
-    private ArrayList<Integer> list = new ArrayList<Integer>();
-    private ArrayList<Integer> list2 = new ArrayList<Integer>();
+    private ArrayList<Integer> distanceList = new ArrayList<Integer>();
+    private ArrayList<Integer> priceList = new ArrayList<Integer>();
     private Iterator<Integer> it;
     private long sum = 0;
 
     public  void run() throws Exception {
         int count = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        int firstDistance = Integer.parseInt(st.nextToken());
-        int distacneSum = 0;
-        for(int i=0; i<count -2; i++)
-            distacneSum+=Integer.parseInt(st.nextToken());
+        for(int i=0; i<count -1; i++)
+            distanceList.add(Integer.parseInt(st.nextToken()));
+
         st = new StringTokenizer(br.readLine());
-        int firstPrice = Integer.parseInt(st.nextToken());
-        for(int i=0; i<count-2; i++)
-            list.add(Integer.parseInt(st.nextToken()));
-        int minPrice = Collections.min(list);
-        sum += firstDistance*firstPrice;
-        sum+=distacneSum*minPrice;
+        for(int i=0; i<count; i++)
+            priceList.add(Integer.parseInt(st.nextToken()));
+
+        int minPrice = priceList.get(0);
+        for(int i=0; i<count-1; i++){
+            int price = priceList.get(i);
+            int distance = distanceList.get(i);
+            if(minPrice > price)
+                minPrice = price;
+            sum+= minPrice * distance;
+        }
+
         System.out.println(sum);
     }
 
