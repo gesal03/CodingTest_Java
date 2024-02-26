cd6454762a4a5d0c0170d49c444bff3ae6a64087
Hyun seung Lee <gesal0303@gmail.com>
2024-02-26 18:47:11 +0900

    [Bronze V] Title: 카드 게임, Time: 124 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.module.FindException;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    private StringBuilder sb = new StringBuilder();
+
+    private int sum = 0;
+    
+
+
+    public  void run() throws Exception {
+        for(int i=0; i<5; i++) sum+= Integer.parseInt(br.readLine());
+
+        System.out.println(sum);
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        new Main().run();
+    }
+}
