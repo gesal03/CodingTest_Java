417f1283b78d3963711f4b4a8094195e31be6430
Hyun seung Lee <gesal0303@gmail.com>
2024-03-05 19:58:43 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 120 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private StringBuilder sb = new StringBuilder();
+
+
+    public  void run() throws Exception {
+        sb.append("비와이");
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception{
+        new Main().run();
+    }
+}
