3c38aa2272bf906ff4e24d9984dd64099fa30753
Hyun seung Lee <gesal0303@gmail.com>
2024-03-31 21:39:00 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 34864 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private StringBuilder sb = new StringBuilder();
+    private int N;
+    private int M;
+    private boolean [] visited;
+    private int [] sequence;
+    private void dfs(int depth){
+        if(depth == M){
+            for(int val : sequence)
+                sb.append(val + " ");
+            sb.append("\n");
+            return;
+        }
+        for(int i=0; i<N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                sequence[depth] = i+1;
+                dfs(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+
+    public void run() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N+1];
+        sequence = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+
+
+
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+}
