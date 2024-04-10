e4c773bd568de5b2814e9e6b5e357267fe1e06e3
Hyun seung Lee <gesal0303@gmail.com>
2024-04-10 19:46:01 +0900

    [Silver II] Title: N과 M (9), Time: 196 ms, Memory: 21532 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private final StringBuilder sb = new StringBuilder();
+    private int N;
+    private int M;
+    private boolean[] visited;
+    private int[] numbers;
+    private int[] sequence;
+
+    private void dfs(int depth) {
+        if (depth == M) {
+            for (int value : sequence) sb.append(value).append(" ");
+            sb.append("\n");
+            return;
+        }
+        int before = 0;
+        for (int i = 0; i < N; i++) {
+            if(visited[i]) continue;
+            if (before != numbers[i]) {
+                visited[i] = true;
+                sequence[depth] = numbers[i];
+                before = numbers[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public void run() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+        numbers = new int[N];
+        sequence = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+        dfs(0);
+        System.out.println(sb);
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+}
