ba30cf0593ecc666bf7e39c8e5ad12526693377f
Hyun seung Lee <gesal0303@gmail.com>
2024-04-22 17:34:01 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private final StringBuilder sb = new StringBuilder();
+    public void run() throws Exception {
+        String str = br.readLine();
+        sb.append(":fan::fan::fan:").append("\n");
+        sb.append(":fan:").append(":").append(str).append(":").append(":fan:").append("\n");
+        sb.append(":fan::fan::fan:").append("\n");
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+}
