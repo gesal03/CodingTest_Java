617ea3cf6bb8c240f559e4a00a6cad52212a88a9
Hyun seung Lee <gesal0303@gmail.com>
2024-04-25 17:55:45 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+    private final StringBuilder sb = new StringBuilder();
+
+    public void run() throws Exception {
+        sb.append("1\n").append("0");
+        System.out.println(sb);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+}
