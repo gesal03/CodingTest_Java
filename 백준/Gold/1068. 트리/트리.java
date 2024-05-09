3db93cc62273860d843b1f193a7ccb46c677b7b3
Hyun seung Lee <gesal0303@gmail.com>
2024-05-09 13:59:14 +0900

    [Gold V] Title: 트리, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -25,24 +25,30 @@ public class Main {
         }
     }
     public void run() throws Exception {
+        // Node의 개수
         int T = Integer.parseInt(br.readLine());
+        // 방문처리 배열 초기화
         visited = new boolean[T];
+        // 트리 배열 초기화
         adjList = new LinkedList[T];
         for (int i = 0; i < T; i++) adjList[i] = new LinkedList<>();
 
+        // 트리 입력
         st = new StringTokenizer(br.readLine());
+        // 삭제할 노드
         deletedNode = Integer.parseInt(br.readLine());
+        // 시작 노드
         int startNode=0;
         for (int i = 0; i < T; i++) {
             int parentNode = Integer.parseInt(st.nextToken());
             if(parentNode == -1)
                 startNode = i;
+            // 삭제할 노드를 애초에 트리에 넣지 않움.
             else if(parentNode != deletedNode && i != deletedNode)
                 adjList[parentNode].add(i);
         }
+        // 트리 각 노드 정렬
         for (int i = 0; i < T; i++) Collections.sort(adjList[i]);
-
-//        adjList[deletedNode].clear();
 
         dfs(startNode);
         System.out.println(count);
