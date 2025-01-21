5aa1f8342c4e88641b52dca93ee65df0012a5117
Hyun seung Lee <gesal0303@gmail.com>
2025-01-21 17:49:38 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	int x,y;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	
+	if(x>0){
+		if (y>0){
+			printf("1");
+		}
+		else if (y<0){
+			printf("4");
+		}	
+		
+	}
+	
+	else if(x<0){
+		if(y>0){
+			printf("2");
+			
+		}
+		else if(y<0){
+			printf("3");
+		}
+	}
+	return 0;
+}
